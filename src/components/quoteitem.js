7e2bfe6088b9c01b1ab7626bcea054087879fd44
Meshack <mbuvi.meshack99@gmail.com>
2022-10-07 19:20:54 +0300

    added few elements

--- src/components/quoteitem.js
+++ src/components/quoteitem.js
@@ -4,6 +4,13 @@ function Quoteitem ( { quote ,clickFunction }) {
     const { id, author, quotees } = quote;
 
     return(
-
+        <>
+        <h2>{author}</h2>
+        <p>{quotees}</p>
+        <button
+        onClick={() => clickFunction(id)}
+        >I Don't like this quote</button>
+        {/* {author === "Life" ? <button>Edit Qoute</button> : null} */}
+    </>
     )
 }
