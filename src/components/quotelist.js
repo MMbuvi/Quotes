9ff5dda867510da8671a0786031736c0e9a8efd0
Meshack <mbuvi.meshack99@gmail.com>
2022-10-07 19:25:28 +0300

    mapped through each item

--- src/components/quotelist.js
+++ src/components/quotelist.js
@@ -2,6 +2,12 @@ import React from "react";
 import Quoteitem from "./quoteitem";
 
 function Quotelist( { quotes , clickFunction  }){
+    return (
     <>
         <h1>Quotes From DB : </h1>
+        {quotes.map((item) => {
+                return  <Quoteitem key={item.id} quote={item} clickFunction={clickFunction}/>
+        })}
+        </>
+    )
 }
