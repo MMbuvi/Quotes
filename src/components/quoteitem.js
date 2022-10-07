873d95481753136594772c0df42260d58549aba0
Meshack <mbuvi.meshack99@gmail.com>
2022-10-07 19:19:45 +0300

    added a quote component

--- src/components/quoteitem.js
+++ src/components/quoteitem.js
@@ -1,18 +1,9 @@
 import React from "react";
 
-function Quoteitem ( { quote ,clickFunction  }) {
+function Quoteitem ( { quote ,clickFunction }) {
     const { id, author, quotees } = quote;
 
-    return (
-        <>
-            <h2 style = {{color:"green"}} >{author}</h2>
-            <p  style ={{color:"black"}}>{quotees}</p>
-            <button
-            onClick={() => clickFunction(id)}
-            >I Don't like this quote</button>
-            {/* {author === "Life" ? <button>Edit Qoute</button> : null} */}
-        </>
+    return(
+
     )
 }
-
-export default Quoteitem;
